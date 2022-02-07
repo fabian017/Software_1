e39d0c36b6d868892886b9dc8b01e875a397d365
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2022-02-06 21:33:30 -0500

    postes

---
+++ modelo_postes/Sistema_postes/src/main/java/Clases/Componente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Clases;
+
+/**
+ *
+ * @author FABIAN <fabian.jovalle at gmail.com>
+ */
+public abstract class Componente {
+    //ATRIBUTOS
+    private String nombre;
+    
+    //CONSTRUCTOR
+    public Componente(){
+        nombre = "";
+    }
+    
+    public Componente(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //GETTERS AND SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     
+    //METODOS
+    
+}
---
+++ modelo_postes/Sistema_postes/src/main/java/Clases/LineaAltaTension.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Clases;
+
+/**
+ *
+ * @author FABIAN <fabian.jovalle at gmail.com>
+ */
+public class LineaAltaTension extends Componente {
+    private Componente componente;
+    
+    public LineaAltaTension(Componente componente) {
+        this.componente = componente;
+    }
+
+    public LineaAltaTension(Componente componente, String nombre) {
+        super(nombre);
+        this.componente = componente;
+    }
+
+    public Componente getComponente() {
+        return componente;
+    }
+
+    public void setComponente(Componente componente) {
+        this.componente = componente;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString()+"Transformador{" + "componente=" + componente + '}';
+    }
+}
