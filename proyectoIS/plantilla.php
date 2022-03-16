8c680ebf7848e7a8b3d7d7b95d2388b90df94d1b
julianmantilla <67700245+julianmantilla@users.noreply.github.com>
2022-03-16 16:56:04 -0500

    instagram facebook y twitter en plantilla

--- proyectoIS/plantilla.php
+++ proyectoIS/plantilla.php
@@ -324,16 +324,16 @@
             <div class="contenedor">
             <div class="iconos">
                 <div class="iconos-centrar icon">
-                <i class="fab fa-facebook fa-4x"></i>
+                <a href="https://es-la.facebook.com/AtleticoBucaramangaOficial"> <i class="fab fa-facebook fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
-                <i class="fab fa-twitter-square fa-4x"></i>
+                <a href="https://twitter.com/ABucaramanga?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter-square fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
                 <i class="fas fa-phone-square-alt fa-4x"></i>
                 </div>
                 <div class="iconos-centrar icon">
-                <i class="fab fa-instagram-square fa-4x"></i>
+                <a href="https://www.instagram.com/bucaramangaoficial/?hl=es"><i class="fab fa-instagram-square fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
                 <i class="fas fa-search-location fa-4x"></i>
