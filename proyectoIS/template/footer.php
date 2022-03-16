6dde5de01f8b93d92d321793b8f3d56b1fd69dc9
julianmantilla <67700245+julianmantilla@users.noreply.github.com>
2022-03-16 17:14:47 -0500

    a

--- proyectoIS/template/footer.php
+++ proyectoIS/template/footer.php
@@ -6,16 +6,16 @@
             <div class="contenedor">
             <div class="iconos">
                 <div class="iconos-centrar icon">
-                <a href="https://es-la.facebook.com/AtleticoBucaramangaOficial"> <i class="fab fa-facebook fa-4x"></i></a>
+                <a href="https://es-la.facebook.com/AtleticoBucaramangaOficial" target="_blank"> <i class="fab fa-facebook fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
-                <a href="https://twitter.com/ABucaramanga?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter-square fa-4x"></i></a>
+                <a href="https://twitter.com/ABucaramanga?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><i class="fab fa-twitter-square fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
                 <i class="fas fa-phone-square-alt fa-4x"></i>
                 </div>
                 <div class="iconos-centrar icon">
-                <a href="https://www.instagram.com/bucaramangaoficial/?hl=es"><i class="fab fa-instagram-square fa-4x"></i></a>
+                <a href="https://www.instagram.com/bucaramangaoficial/?hl=es" target="_blank"><i class="fab fa-instagram-square fa-4x"></i></a>
                 </div>
                 <div class="iconos-centrar icon">
                 <i class="fas fa-search-location fa-4x"></i>
